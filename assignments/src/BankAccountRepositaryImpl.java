508eb758d77436315669e8d84b4773d629d0b80c
dattatridunge <107117362+dattatridunge@users.noreply.github.com>
2022-07-04 15:48:22 +0530

    Create BankAccountRepositaryImpl.java

---
+++ assignments/src/BankAccountRepositaryImpl.java
@@ -0,0 +1,17 @@
+package com.bankAccount;
+
+public class BankAccountrepositoryImpl implements BankAccountRepository {
+
+	@Override
+	public double getBalance(long accountId) {
+		
+		return 0;
+	}
+
+	@Override
+	public double updateBalance(long accountId, double newbalance) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
