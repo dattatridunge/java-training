58673c4fcd346d391dc05ee2de6cf10ff04c36a0
dattatridunge <107117362+dattatridunge@users.noreply.github.com>
2022-07-04 16:22:07 +0530

    Create DataConfig.java

---
+++ assignments/src/DataConfig.java
@@ -0,0 +1,14 @@
+package com.springAssignments;
+
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan("com.spring.assignment6")
+@PropertySource("classpath:config.properties")
+public class DataConfig {
+
+}
